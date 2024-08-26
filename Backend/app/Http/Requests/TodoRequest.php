1f348155f72042662efde97bad4f6fe346d67470
Lala <ace.rexnoir@gmail.com>
2024-08-26 19:16:32 +0530

    feat : request validator for Todo

---
+++ Backend/app/Http/Requests/TodoRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TodoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'completed' => 'nullable|boolean',
+            'due_date' => 'nullable|date',
+            'priority' => 'nullable|integer',
+        ];
+    }
+}
