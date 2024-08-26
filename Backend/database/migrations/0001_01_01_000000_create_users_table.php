b18d5b81003977baa08c1834c69656d8b2663ae1
Lala <ace.rexnoir@gmail.com>
2024-08-26 13:48:10 +0530

    fix : remove unnecessary foreignId for Todo

--- Backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignIdFor(Todo::class);
             $table->rememberToken();
             $table->timestamps();
         });
