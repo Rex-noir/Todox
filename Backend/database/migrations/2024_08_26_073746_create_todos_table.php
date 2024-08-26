ca959f477b605a2ce5450783f666fd511ea78331
Lala <ace.rexnoir@gmail.com>
2024-08-26 13:21:20 +0530

    feat : Todo model, migration, seeder,controller and factory

---
+++ Backend/database/migrations/2024_08_26_073746_create_todos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->text('description')->nullable();
+            $table->boolean('completed')->default(false);
+            $table->date('due_date')->nullable();
+            $table->integer('priority')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todos');
+    }
+};
