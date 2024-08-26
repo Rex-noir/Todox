8002f0bb1f198834deb16c580fe0162a07c366bf
Lala <ace.rexnoir@gmail.com>
2024-08-26 13:23:24 +0530

    feat : relationship with Todo Model

--- Backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Todo;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignIdFor(Todo::class);
             $table->rememberToken();
             $table->timestamps();
         });
