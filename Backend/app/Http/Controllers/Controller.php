4365155c9a1f846288abdb03c39c9ff069a92bd2
Lala <ace.rexnoir@gmail.com>
2024-08-26 19:15:59 +0530

    feat : use AuthorizeRequest trait

--- Backend/app/Http/Controllers/Controller.php
+++ Backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+abstract class Controller extends \Illuminate\Routing\Controller
 {
-    //
+    use AuthorizesRequests;
 }
