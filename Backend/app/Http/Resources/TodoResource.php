f3123d9ac520548a258d8166623e8eb54d854665
Lala <ace.rexnoir@gmail.com>
2024-08-26 19:16:49 +0530

    feat : todo api resource

---
+++ Backend/app/Http/Resources/TodoResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TodoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'completed' => $this->completed,
+            'due_date' => $this->due_date,
+            'priority' => $this->priority,
+        ];
+    }
+}
