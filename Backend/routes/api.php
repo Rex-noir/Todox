7dd423d54773f5e9d4981d702ae1a2e0a6e25751
Lala <ace.rexnoir@gmail.com>
2024-08-26 16:28:22 +0530

    feat : define api resource route for todos

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('todos', TodoController::class);
+});
