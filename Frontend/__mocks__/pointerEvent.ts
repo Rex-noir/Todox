ab5779d2ee56133964f5a9ba126f2c37fe7633fb
Lala <ace.rexnoir@gmail.com>
2024-08-28 22:01:46 +0530

    feat : mock for pointer event

---
+++ Frontend/__mocks__/pointerEvent.ts
@@ -0,0 +1,12 @@
+export default class MockPointerEvent extends Event {
+  button: number;
+  ctrlKey: boolean;
+  pointerType: string;
+
+  constructor(type: string, props: PointerEventInit) {
+    super(type, props);
+    this.button = props.button || 0;
+    this.ctrlKey = props.ctrlKey || false;
+    this.pointerType = props.pointerType || "mouse";
+  }
+}
