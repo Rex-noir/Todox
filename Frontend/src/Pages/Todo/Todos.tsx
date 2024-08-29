0b7bdf021425df91663d2dd63004806d3aef1644
Lala <ace.rexnoir@gmail.com>
2024-08-30 01:11:57 +0530

    refactor : remove javascript , use tailwind only

--- Frontend/src/Pages/Todo/Todos.tsx
+++ Frontend/src/Pages/Todo/Todos.tsx
@@ -1,34 +1,18 @@
-import { useState, useEffect } from "react";
 import MobileHeader from "./MobileHeader";
 import LayoutMenu from "./LargeScreenMenu";
 import ViewManager from "./ViewManager";
 
 export const Todos = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      {!isMobile && (
-        <div className="fixed left-0 top-0 h-full w-80 bg-white shadow-lg">
-          <LayoutMenu />
+    <div className="grid min-h-screen bg-gray-100 md:grid-cols-[18rem,1fr]">
+      <div className="fixed left-0 top-0 hidden h-full bg-white shadow-lg md:block md:w-72 lg:w-80">
+        <LayoutMenu />
+      </div>
+      <div className="grid grid-rows-[74px,1fr] md:col-start-2 md:grid-rows-1">
+        <div className="fixed left-0 right-0 top-0 z-10 md:hidden">
+          <MobileHeader />
         </div>
-      )}
-      <div className={`flex-1 ${!isMobile ? "ml-80" : ""}`}>
-        {isMobile && (
-          <div className="fixed left-0 right-0 top-0 z-10">
-            <MobileHeader />
-          </div>
-        )}
-        <main className={`${isMobile ? "mt-16" : ""}`}>
+        <main className="row-start-2 md:row-start-1">
           <ViewManager />
         </main>
       </div>
