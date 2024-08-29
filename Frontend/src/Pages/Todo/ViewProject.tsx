0519e738a5231adf98bd9c27984e25be2a5619db
Lala <ace.rexnoir@gmail.com>
2024-08-30 01:13:37 +0530

    refactor : improve todoListITem design

--- Frontend/src/Pages/Todo/ViewProject.tsx
+++ Frontend/src/Pages/Todo/ViewProject.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardContent,
@@ -7,6 +8,7 @@ import {
 } from "@/components/ui/card";
 import { Project, TodoList } from "@/interfaces/types";
 import { useState } from "react";
+import { LuListTodo } from "react-icons/lu";
 
 export default function ViewProject() {
   const [project, setProject] = useState<Project | null>(null);
@@ -21,10 +23,18 @@ export default function ViewProject() {
   );
 }
 
-export function TodoListItem({list}:{list:TodoList}){
+export function TodoListItem({ list }: { list: TodoList }) {
   return (
-    <div>
-      Todoslist 
+    <div className="flex cursor-pointer gap-3 overflow-hidden rounded-md bg-white p-3 hover:bg-neutral-100">
+      <div className="flex items-start gap-4">
+        <LuListTodo className="mt-1 size-5 text-blue-500" />
+      </div>
+      <div className="grid w-full items-center gap-1 sm:gap-3 md:grid-cols-3">
+        <div className="w-fit max-w-full overflow-hidden">
+          <p className="min-w-0 flex-1 truncate">{list.title}</p>
+          <p className="truncate text-sm font-thin">{list.description}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
