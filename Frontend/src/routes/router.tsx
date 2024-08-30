f901f1e90eb8858087a5bb8a4876f4a781daa2d9
Lala <ace.rexnoir@gmail.com>
2024-08-30 22:57:44 +0530

    refactor : add dynamic url with viewTypes and projectId

--- Frontend/src/routes/router.tsx
+++ Frontend/src/routes/router.tsx
@@ -1,15 +1,33 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Todos } from "@/Pages/Todo/Todos";
-import { createBrowserRouter } from "react-router-dom";
+import { ViewProject } from "@/Pages/ViewProject";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
   },
-
   {
-    path: "/todos",
+    path: "/app",
     element: <Todos />,
+    children: [
+      {
+        path: "/app/projects",
+        element: <Navigate to={"/app/today"} />,
+      },
+      {
+        path: "/app/:viewType/:projectId",
+        element: <ViewProject />,
+      },
+      {
+        path: "/app/:viewType",
+        element: <ViewProject />,
+      },
+      {
+        path: "/app/:viewType",
+        element: <ViewProject />,
+      },
+    ],
   },
 ]);
 
