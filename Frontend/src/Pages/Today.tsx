862722ab62a5ce092e789ab83f4fdb1207b8cfd4
Lala <ace.rexnoir@gmail.com>
2024-08-31 10:32:25 +0530

    initial commit

---
+++ Frontend/src/Pages/Today.tsx
@@ -0,0 +1,34 @@
+import { useTodoxStore } from "@/stores/todox/todoxStore";
+import { TodoItem } from "./ViewProject";
+import sortTodos from "@/lib/sortTodos";
+import { CheckCircledIcon } from "@radix-ui/react-icons";
+import CreateNewTodo from "@/components/custom/CreateNewTodoButton";
+
+export function TodayPage() {
+  const todos = useTodoxStore((state) => state.todos);
+
+  const sortedTodos = sortTodos(Object.values(todos));
+  const completed = sortedTodos.filter((todo) => todo.completed);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2 px-2">
+        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
+          Today
+        </h1>
+        <p className="flex items-center gap-2 px-1 text-gray-600">
+          <CheckCircledIcon /> {completed.length} of {sortedTodos.length}{" "}
+          completed
+        </p>
+        <div>
+          <CreateNewTodo />
+        </div>
+      </div>
+      {sortedTodos.map((todo) => (
+        <div key={todo.id} className="border-b">
+          <TodoItem todo={todo} />
+        </div>
+      ))}
+    </div>
+  );
+}
