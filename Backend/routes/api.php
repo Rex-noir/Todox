a668f4a6e0b356a2d5482b6a2737ed15a8ff270b
Lala <ace.rexnoir@gmail.com>
2024-09-01 22:12:31 +0530

    feat : define route for login, register and logout

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -10,6 +10,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+})->middleware(['web']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('todos', TodoController::class);
     Route::apiResource('projects', ProjectController::class);
