5204e98e18bee8e1e526cdc0fd0325fff6bd2041
Lala <ace.rexnoir@gmail.com>
2024-09-02 13:05:07 +0530

    initial commit

---
+++ Frontend/src/Pages/Auth/RegisterPage.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
+export default function RegisterPage() {
+  return (
+    <div className="mx-auto grid w-[350px] gap-6">
+      <div className="grid gap-2 text-center">
+        <h1 className="text-3xl font-bold">Register</h1>
+        <p className="text-balance text-muted-foreground">
+          Fill the required fields to register.
+        </p>
+      </div>
+      <div className="grid gap-4">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <Link
+              to="/forgot-password"
+              className="ml-auto inline-block text-sm underline"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Register
+        </Button>
+        <Button variant="outline" className="w-full">
+          Register with Google
+        </Button>
+      </div>
+      <div className="mt-4 text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <Link to="/auth/login" className="underline">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+}
