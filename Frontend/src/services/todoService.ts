77e58b63b5a7a5e131dbfccb75d1f15479b25a11
Lala <ace.rexnoir@gmail.com>
2024-09-02 23:50:00 +0530

    feat : implement creating todo request

--- Frontend/src/services/todoService.ts
+++ Frontend/src/services/todoService.ts
@@ -1,14 +1,16 @@
 import api from "@/api";
-import { Todo } from "@/interfaces/types";
-import { deleteTodo, updateTodo } from "@/stores/todox/actions";
+import { ApiResponse, Todo } from "@/interfaces/types";
+import { addTodo, deleteTodo, updateTodo } from "@/stores/todox/actions";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 
 export const todoServices = {
   delete: (id: string) => api.delete(`/todos/${id}`),
   getAll: (): Promise<Todo[]> => api.get("/todos"),
   getById: (id: string) => api.get(`/todos/${id}`),
   update: (id: string, todo: Partial<Todo>) => api.put(`/todos/${id}`, todo),
-  create: (todo: Partial<Todo>) => api.post("/todos", todo),
+  create: (todo: Partial<Todo>): Promise<AxiosResponse<ApiResponse<Todo>>> =>
+    api.post("/todos", todo),
 };
 
 export const useDeleteTodo = (id: string) => {
@@ -41,6 +43,15 @@ export const useUpdateTodo = (id: string, todo: Partial<Todo>) => {
     mutationFn: () => todoServices.update(id, todo),
     onSuccess: () => {
       updateTodo(id, todo);
+    },
+  });
+};
+
+export const useCreateTodo = () => {
+  return useMutation({
+    mutationFn: (todo: Partial<Todo>) => todoServices.create(todo),
+    onSuccess: (data) => {
+      addTodo(data.data.data);
     },
   });
 };
