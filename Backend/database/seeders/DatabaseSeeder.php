336d5b7af79d1a9658ac1d395277bd7b364962d1
Lala <ace.rexnoir@gmail.com>
2024-09-02 20:14:43 +0530

    seeding implemented

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\Todo;
+use App\Models\TodoList;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +18,24 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Project::factory(rand(4, 10))->for($user)->create()->each(function (Project $project) use ($user) {
+            TodoList::factory(rand(1, 10))
+                ->for($project)
+                ->for($user)
+                ->create()
+                ->each(function (TodoList $todoList) use ($user) {
+                    Todo::factory(rand(1, 10))
+                        ->for($todoList)
+                        ->for($user)
+                        ->create();
+
+                    Todo::factory()->dueToday()->withUser($user)->create();
+                });
+        });
     }
 }
