44fd78ce4f9e399e91b9461b4fc6c434bed7ef7e
Lala <ace.rexnoir@gmail.com>
2024-09-02 13:07:15 +0530

    feat : define route for authentication

--- Frontend/src/routes/router.tsx
+++ Frontend/src/routes/router.tsx
@@ -1,17 +1,26 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import { Todos } from "@/Pages/Todo/Todos";
+import Todos from "@/Pages/Todo/Todos";
 import { ViewProject } from "@/Pages/ViewProject";
 import { TodayPage } from "@/Pages/Today";
+import { Auth } from "@/Pages/Auth/Auth";
+import { LoginPage } from "@/Pages/Auth/LoginPage";
+import RegisterPage from "@/Pages/Auth/RegisterPage";
+import { authRequired } from "@/utils/routeLoaders";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Navigate to={"/app"} />,
   },
   {
     path: "/app",
     element: <Todos />,
+    loader: authRequired,
     children: [
+      {
+        path: "",
+        element: <Navigate to={"/app/today"} />,
+      },
       {
         path: "/app/projects",
         element: <Navigate to={"/app/today"} />,
@@ -27,6 +36,24 @@ const router = createBrowserRouter([
       {
         path: "/app/important",
         element: <TodayPage />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+    children: [
+      {
+        path: "",
+        element: <LoginPage />,
+      },
+      {
+        path: "/auth/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/auth/register",
+        element: <RegisterPage />,
       },
     ],
   },
