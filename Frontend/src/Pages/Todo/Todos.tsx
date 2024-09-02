2b655a4ae2538cf5921af3174232a5e03b1dc69b
Lala <ace.rexnoir@gmail.com>
2024-09-02 20:29:40 +0530

    feat : initial loading data if authenticated

--- Frontend/src/Pages/Todo/Todos.tsx
+++ Frontend/src/Pages/Todo/Todos.tsx
@@ -17,6 +17,11 @@ import Splash from "../Splash";
 import LargeScreenMenu from "./LargeScreenMenu";
 import MenuToggleButton from "@/components/MenuToggleButton";
 import LogoutButton from "@/components/custom/LogoutButton";
+import { ApiResponse, Project, Todo, TodoList, User } from "@/interfaces/types";
+import { setProjects, setTodoLists, setTodos } from "@/stores/todox/actions";
+import { setUser } from "@/stores/auth/actions";
+import { AxiosResponse } from "axios";
+import { useMediaQuery } from "@/lib/hooks/useMediaQuery";
 
 const SideMenu: React.FC<{ isOpen: boolean; toggleMenu: () => void }> = ({
   isOpen,
@@ -29,13 +34,9 @@ const SideMenu: React.FC<{ isOpen: boolean; toggleMenu: () => void }> = ({
         animate={{ x: 0 }}
         exit={{ x: "-100%" }}
         transition={{ type: "spring", stiffness: 300, damping: 30 }}
-        className="fixed left-0 top-0 z-20 h-full w-80 bg-white md:w-[20rem]"
+        className="fixed left-0 top-0 z-50 flex h-full w-80 flex-col bg-white md:w-[20rem]"
       >
-        <Button
-          onClick={toggleMenu}
-          className="absolute right-2 top-2"
-          variant="ghost"
-        >
+        <Button onClick={toggleMenu} className="self-end" variant="ghost">
           <LuPanelLeft size={24} />
         </Button>
         <LargeScreenMenu />
@@ -62,11 +63,12 @@ const Header: React.FC<{
   return (
     <header
       className={cx(
-        "fixed z-40 flex items-center p-2 transition-all duration-300",
+        "fixed z-40 flex items-center justify-between p-2 transition-all duration-300",
         {
           "bg-white shadow-sm": isScrolled,
           "w-full": !isMenuOpen,
           "w-[calc(100%-20rem)]": isMenuOpen,
+          "ml-[20rem]": isMenuOpen,
         },
       )}
     >
@@ -81,15 +83,34 @@ const Header: React.FC<{
       >
         {getTitle()}
       </motion.h1>
-      <LogoutButton/>
+      <LogoutButton />
     </header>
   );
 };
 
 const ResponsiveLayout: React.FC = () => {
-  const { authStatus } = useLoaderData() as { authStatus: boolean };
+  const { values } = useLoaderData() as {
+    values: Promise<
+      [
+        AxiosResponse<ApiResponse<User>>,
+        AxiosResponse<ApiResponse<Project[]>>,
+        AxiosResponse<ApiResponse<TodoList[]>>,
+        AxiosResponse<ApiResponse<Todo[]>>,
+      ]
+    >;
+  };
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
+
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (isMobile) {
+      setIsMenuOpen(false);
+    }
+  }, [location, isMobile]);
 
   useEffect(() => {
     const handleScroll = () => setIsScrolled(window.scrollY > 20);
@@ -97,35 +118,45 @@ const ResponsiveLayout: React.FC = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  useEffect(() => {
+    values.then((v) => {
+      const [user, projects, todoLists, todos] = v;
+      setProjects(projects.data.data);
+      setTodoLists(todoLists.data.data);
+      setTodos(todos.data.data);
+      setUser(user.data.data);
+    });
+  },[values]);
+
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
     <Suspense fallback={<Splash />}>
-      <Await resolve={authStatus} errorElement={<Navigate to="/auth/login" />}>
-        {() => (
-          <div className="flex min-h-screen">
-            <SideMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
-            <div className="relative flex-1">
-              <div className={cx({ "ml-[20rem]": isMenuOpen })}>
+      <Await resolve={values} errorElement={<Navigate to="/auth/login" />}>
+        {() => {
+          return (
+            <div className="flex min-h-screen">
+              <SideMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
+              <div className="relative flex-1">
                 <Header
                   isScrolled={isScrolled}
                   isMenuOpen={isMenuOpen}
                   toggleMenu={toggleMenu}
                 />
+                <main
+                  className={cx(
+                    "mt-14 flex min-h-screen justify-center p-4 transition-all duration-300",
+                    {
+                      "md:ml-[20rem]": isMenuOpen,
+                    },
+                  )}
+                >
+                  <Outlet />
+                </main>
               </div>
-              <main
-                className={cx(
-                  "mt-14 flex min-h-screen justify-center p-4 transition-all duration-300",
-                  {
-                    "md:ml-[20rem]": isMenuOpen,
-                  },
-                )}
-              >
-                <Outlet />
-              </main>
             </div>
-          </div>
-        )}
+          );
+        }}
       </Await>
     </Suspense>
   );
