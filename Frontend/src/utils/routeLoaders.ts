855c5d178cdf0b355d5b1929270d7309d3989fec
Lala <ace.rexnoir@gmail.com>
2024-09-02 20:30:31 +0530

    refactor : defer all data loading request

--- Frontend/src/utils/routeLoaders.ts
+++ Frontend/src/utils/routeLoaders.ts
@@ -1,17 +1,13 @@
 import api from "@/api";
-import { isAuthenticated, setUser } from "@/stores/auth/actions";
 import { defer } from "react-router-dom";
 
 export const authRequired = () => {
+  const getUser = api.get("/user");
+  const getProjects = api.get("/projects");
+  const getTodoLists = api.get("/todolists");
+  const getTodos = api.get("/todos");
+
   return defer({
-    authStatus: (async () => {
-      const status = isAuthenticated();
-      if (!status) {
-        const res = await api.get("/user");
-        setUser(res.data);
-        return true;
-      }
-      return true;
-    })(),
+    values: Promise.all([getUser, getProjects, getTodoLists, getTodos]),
   });
 };
