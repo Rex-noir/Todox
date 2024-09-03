cf189ce3a0dd3ff9f237bfd5e18c1de09dd2a88f
Lala <ace.rexnoir@gmail.com>
2024-09-04 00:28:36 +0530

    add placeholder text

--- Frontend/src/Pages/Auth/Auth.tsx
+++ Frontend/src/Pages/Auth/Auth.tsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom";
 
-export const description =
-  "A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image.";
-
 export function Auth() {
   return (
-    <div className="w-full px-3 lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="h-screen max-h-screen w-full px-3 lg:grid lg:grid-cols-2">
       <div className="flex items-center justify-center py-12">
         <Outlet />
       </div>
-      <div className="hidden bg-muted lg:block">
-        <p>Placeholder</p>
+      <div className="hidden flex-col items-center justify-center bg-muted lg:flex">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Todox{" "}
+        </h1>{" "}
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+          Simple | Elegant | OS
+        </p>{" "}
       </div>
     </div>
   );
