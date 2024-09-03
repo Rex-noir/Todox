4258a48b7cea578a9db045b1eb0da21eb7e954d6
Lala <ace.rexnoir@gmail.com>
2024-09-03 10:10:57 +0530

    refactor : change dialog to collapsible feat : implement adding new todolist

--- Frontend/src/components/custom/CreateNewTodolistButton.tsx
+++ Frontend/src/components/custom/CreateNewTodolistButton.tsx
@@ -1,50 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
 import { Input } from "../ui/input";
-import { Label } from "../ui/label";
 import { Textarea } from "../ui/textarea";
-import { addTodoList } from "@/stores/todox/actions";
-import { faker } from "@faker-js/faker";
 import { useParams } from "react-router-dom";
 import { PlusIcon } from "@radix-ui/react-icons";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible";
+import { Badge } from "../ui/badge";
+import { useCreateTodoList } from "@/services/todoListService";
+import { TbLoaderQuarter } from "react-icons/tb";
 
 export function CreateNewTodoList() {
-  const [isOpen, setIsOpen] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [tags, setTags] = useState<string[]>([]);
+
   const { projectId } = useParams();
+  const useTodoListMutation = useCreateTodoList();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    addTodoList({
-      id: faker.string.uuid(),
+
+    const newTodoList = {
       title,
       description,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      completedTodosCount: 0,
-      incompleteTodosCount: 0,
-      todoIds: [],
-      project_id: projectId || "",
-    });
+      project_id: projectId ?? "",
+      tags: JSON.stringify(tags),
+    };
 
-    setIsOpen(false);
+    useTodoListMutation.mutate(newTodoList);
 
     setTitle("");
+    setTags([]);
     setDescription("");
   };
+
+  useEffect(() => {
+    if (useTodoListMutation.isSuccess) {
+      setIsOpen(false);
+    }
+  }, [useTodoListMutation.isSuccess]);
+
+  const handleKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const inputValue = e.currentTarget.value.trim();
+      if (inputValue) {
+        setTags([...tags, inputValue]);
+        e.currentTarget.value = "";
+      }
+    }
+  };
   return (
-    <Dialog defaultOpen={isOpen} onOpenChange={setIsOpen}>
-      <DialogTrigger asChild>
+    <Collapsible open={isOpen} onOpenChange={setIsOpen} className="space-y-2">
+      <CollapsibleTrigger asChild>
         <Button
           className="w-fit items-center gap-3 text-gray-500"
           variant={"link"}
@@ -53,35 +67,60 @@ export function CreateNewTodoList() {
           <PlusIcon />
           <span>Add List</span>
         </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Create new todo list.</DialogTitle>
-          <DialogDescription>Todo is a thing we need to do.</DialogDescription>
-        </DialogHeader>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-2 rounded-md border-2 p-2"
+        >
           <div>
-            <Label htmlFor="title">Title</Label>
             <Input
               value={title}
               onChange={(e) => setTitle(e.target.value)}
               id="title"
+              placeholder="Title"
+              className="border-none text-xl font-semibold shadow-none focus-visible:ring-0"
             />
-          </div>
-          <div>
-            <Label htmlFor="description">Title</Label>
             <Textarea
+              placeholder="Description"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
               id="description"
-              rows={5}
+              rows={3}
+              className="resize-none border-none shadow-none focus-visible:ring-0"
             />
           </div>
-          <div className="flex justify-end">
-            <Button size={"sm"}>Create</Button>
+          <div className="flex flex-col gap-2">
+            <div>
+              Tags:{" "}
+              <div className="mt-1 flex flex-wrap gap-1">
+                {tags.map((tag) => (
+                  <Badge variant={"outline"} key={tag}>
+                    {tag}
+                  </Badge>
+                ))}
+              </div>
+            </div>
+            <Input onKeyDown={handleKeydown} placeholder="Enter tags" />
+          </div>
+          <div className="flex justify-end gap-3">
+            <Button
+              variant={"outline"}
+              onClick={() => setIsOpen(false)}
+              size={"sm"}
+              type="button"
+            >
+              Cancel
+            </Button>
+            <Button type="submit" size={"sm"}>
+              {!useTodoListMutation.isPending && "Create"}
+              {useTodoListMutation.isPending && (
+                <TbLoaderQuarter className="animate-spin" />
+              )}
+            </Button>
           </div>
         </form>
-      </DialogContent>
-    </Dialog>
+      </CollapsibleContent>
+    </Collapsible>
   );
 }
