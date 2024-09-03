7d01a7de7801de157b3d80fdb9b941a6f8756678
Lala <ace.rexnoir@gmail.com>
2024-09-04 00:24:13 +0530

    fix : nesting of button error in dev console

--- Frontend/src/components/custom/TodoListOptions.tsx
+++ Frontend/src/components/custom/TodoListOptions.tsx
@@ -9,29 +9,33 @@ export default function TodoListOptions({ listId }: { listId: string }) {
   const deleteMutation = useDeleteTodoList(listId);
 
   return (
-    <Popover>
-      <PopoverTrigger>
-        <Button size={"sm"} variant={"ghost"}>
-          <GoKebabHorizontal className="size-5 cursor-pointer" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[150px] p-3">
-        <div>
-          <Button
-            onClick={() => deleteMutation.mutate()}
-            variant={"destructive"}
-            className="w-full gap-2"
-            size={"sm"}
-          >
-            <TrashIcon />
-            {deleteMutation.isPending ? (
-              <TbLoader3 className="animate-spin" />
-            ) : (
-              "Delete"
-            )}
-          </Button>
-        </div>
-      </PopoverContent>
-    </Popover>
+    <div key={listId} onClick={(e) => e.stopPropagation()}>
+      <Popover>
+        <PopoverTrigger asChild={true}>
+          <span>
+            <Button asChild size={"sm"} variant={"ghost"}>
+              <GoKebabHorizontal className="size-10 cursor-pointer" />
+            </Button>
+          </span>
+        </PopoverTrigger>
+        <PopoverContent className="w-[150px] p-3">
+          <div>
+            <Button
+              onClick={() => deleteMutation.mutate()}
+              variant={"destructive"}
+              className="w-full gap-2"
+              size={"sm"}
+            >
+              <TrashIcon />
+              {deleteMutation.isPending ? (
+                <TbLoader3 className="animate-spin" />
+              ) : (
+                "Delete"
+              )}
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
