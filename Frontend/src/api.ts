30ef7eaa5a015a52d58ef309149466aa8cec3273
Lala <ace.rexnoir@gmail.com>
2024-09-12 13:07:06 +0530

    feat : handle error globally

--- Frontend/src/api.ts
+++ Frontend/src/api.ts
@@ -1,8 +1,19 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
+import { ApiResponse } from "./interfaces/types";
+import { toast } from "sonner";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   withXSRFToken: true,
 });
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (isAxiosError(error)) {
+      const e = error.response?.data as ApiResponse;
 
+      toast.error(e.message);
+    }
+  },
+);
 export default api;
